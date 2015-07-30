baa85f42a93387f8e0b906b9cd82a5bd8dcaa303
Marek <marek.kaszewski@gmail.com>
2015-07-30 13:05:20 +0100

    preparing app frame

--- src/main/java/com/thingoncloud/web/run/WebApplication.java
+++ src/main/java/com/thingoncloud/web/run/WebApplication.java
@@ -1,31 +1,55 @@
 package com.thingoncloud.web.run;
 
-import spark.servlet.SparkApplication;
-import spark.template.mustache.MustacheTemplateEngine;
-
+import static spark.Spark.exception;
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import spark.Spark;
+import spark.servlet.SparkApplication;
+import spark.template.mustache.MustacheTemplateEngine;
 
 public class WebApplication implements SparkApplication {
 
 	@Override
 	public void init() {
 		
-		Map map = new HashMap();
+		staticFileLocation("public");
+		
+		Map<String, String> map = new HashMap<String, String>();
 		map.put("name", "Sam");
-		get("/hello", (rq, rs) -> new ModelAndView(map, "another/helloqq.mustache"), new MustacheTemplateEngine());
+		get("/hello1", (rq, rs) -> new ModelAndView(map, "another/helloqq.mustache"), new MustacheTemplateEngine());
+		
 
+		// hello.mustache file is in resources/templates directory
+		get("/hello", (rq, rs) -> new ModelAndView(map, "hello.mustache"), new MustacheTemplateEngine());
+		get("/error404", (rq, rs) -> new ModelAndView(map, "errors/error404.mustache"), new MustacheTemplateEngine());
+		get("/error500", (rq, rs) -> new ModelAndView(map, "errors/error500.mustache"), new MustacheTemplateEngine());
+		get("/hello2", (req, res) -> "bla, bla");	
+		get("/login", (rq, rs) -> new ModelAndView(map, "user/standalone_login_register.mustache"), new MustacheTemplateEngine());
+		get("/myText/:id", (request, response) -> {
+            return request.params(":id");
+        });		
+        
+     //   get("*", (rq, rs) -> new ModelAndView(map, "errors/error404.mustache"), new MustacheTemplateEngine());
+              
+        exception(Exception.class, (e, request, response) -> {
+            response.status(500);
+            MustacheTemplateEngine m = new MustacheTemplateEngine();
+            ModelAndView mv = new ModelAndView(map, "errors/error500.mustache");         
+            response.body(m.render(mv));
+        });
+       
+		// get("/hello", (req, res) -> "Hello World");
+		// http://mustache.github.io/mustache.5.html
+        // https://github.com/perwendel/spark
+		
 	}
 
 	public static void main(String[] args) {
-		
+		WebApplication wa = new WebApplication();
+		wa.init();
 	}
 }
